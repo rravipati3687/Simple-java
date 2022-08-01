00607bb918673af28adac03c0550f765fb55906c
rravipati3687 <ramakrishna.ravipati@gmail.com>
2022-08-01 15:47:46 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,6 @@ public class AppTest
     {
         App.main(null);
         try {
-            System.out.println("Hello world");
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
